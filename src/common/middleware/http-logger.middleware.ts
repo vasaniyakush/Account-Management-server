f170d85e508357c41ec0dec8660ff0e59d37ff75
Kush Vasaniya <vasaniyakush@gmail.com>
2025-07-07 17:01:19 +0530

    removed morgan

--- src/common/middleware/http-logger.middleware.ts
+++ src/common/middleware/http-logger.middleware.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import * as morgan from 'morgan';
 import { Request, Response, NextFunction } from 'express';
 
 interface RequestCustom extends Request {
@@ -8,31 +7,28 @@ interface RequestCustom extends Request {
     email: string;
   };
 }
-const logger = (morgan as any).default || morgan;
 
 @Injectable()
 export class HttpLoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(HttpLoggerMiddleware.name);
 
   use(req: RequestCustom, res: Response, next: NextFunction) {
-    logger(
-      ':remote-addr :method :url :status :res[content-length] :response-time ms',
-      {
-        stream: {
-          write: (message: any) => {
-            if (req?.user) {
-              this.logger.log(
-                `${message} > ${req.user?.userId} | ${req.user?.email}`.replace(
-                  /(\r\n|\n|\r)/gm,
-                  '',
-                ),
-              );
-            } else {
-              this.logger.log(message);
-            }
-          },
-        },
-      },
-    )(req, res, next);
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+      const duration = Date.now() - startTime;
+      const contentLength = res.get('Content-Length') || 0;
+      const logMessage = `${req.ip} ${req.method} ${req.originalUrl} ${res.statusCode} ${contentLength}B ${duration}ms`;
+
+      if (req.user) {
+        this.logger.log(
+          `${logMessage} > ${req.user.userId} | ${req.user.email}`,
+        );
+      } else {
+        this.logger.log(logMessage);
+      }
+    });
+
+    next();
   }
 }
